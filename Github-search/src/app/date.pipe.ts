a468a75eb49759ccef6afbe373ed6ed20d3b1316
Serah wakaba <serahwakaba9@gmail.com>
2021-05-24 15:12:25 +0300

    directive commit

--- Github-search/src/app/date.pipe.ts
+++ Github-search/src/app/date.pipe.ts
@@ -1,13 +1,13 @@
-// import { Pipe, PipeTransform } from '@angular/core';
+import { Pipe, PipeTransform } from '@angular/core';
 
-// @Pipe({
-//   name: 'dates'
-// })
-// export class DatePipe implements PipeTransform {
+@Pipe({
+  name: 'dates'
+})
+export class DatePipe implements PipeTransform {
 
-//   transform(value: any, ...args): any {
-//     console.log(value, args)
-//     return Date();
-//   }
+  transform(value: any, ...args): any {
+    console.log(value, args)
+    return Date();
+  }
 
-// }
+}
